1e906e39b39a136e7e4a823ab929687a71c851ef
Lars Jönsson <crymeat@gmail.com>
2020-09-01 22:09:56 +0200

    nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,16 +51,6 @@ module.exports = {
   generate: {
     fallback: "404.html",
   },
-/*   buildModules: [
-    ['@nuxtjs/google-analytics', {
-      id: 'UA-121315851-1'
-    }]
-  ],
-  publicRuntimeConfig: {
-    googleAnalytics: {
-      id: process.env.GOOGLE_ANALYTICS_ID
-    }
-  }, */
   /*
    ** Headers of the page
    */
@@ -138,7 +128,7 @@ module.exports = {
    */
   plugins: [
     { src: "~/plugins/vue-scroll-reveal", ssr: false },
-    { src: "~/plugins/ga.js", mode: 'client' },
+    { src: "~/plugins/ga.js", ssr: false },
     { src: "~/plugins/vee-validate.js", ssr: false }
   ],
 
